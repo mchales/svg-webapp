f7b87ae6b1f0c8b452064f38a4310730c4749540
Sean McHale <seanmchale@Seans-MacBook-Pro-4.local>
2023-04-13 17:38:59 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import model
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile = request.files['imageFile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    caption = model.create_new_caption(image_path)
+    return render_template('index.html', prediction = caption)
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
